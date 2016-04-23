cdbbd1601fe3f85e23bf217eca551529c4efde09
Yongzheng Huang <iamhuangyz@gmail.com>
2016-04-23 14:13:00 -0500

    add log messages

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) { res.render('index'); });
+app.get('/', function(req, res) {
+    console.log('running app');
+    console.log('test');
+    res.render('index');
+});
 
 
 app.listen(app.get('port'), function() {
